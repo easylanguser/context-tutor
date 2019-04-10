f161fd21a3a700bd9011dd03ff18e8d23bede420
ErgerSusha <ergersusha2@gmail.com>
2019-04-10 11:11:43 +0300

    Changed deploy script, updated some packages

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ echo -e "Building www"
 ionic cordova build browser
 echo -e "www has built"
 echo -e "Copying files to vm"
-scp -r www/  root@165.227.159.35:/home/tutor/
+scp -r www/  root@46.101.122.247:/var/www/html
 echo -e "Files have copied"
 
