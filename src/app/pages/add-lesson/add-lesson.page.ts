7dcbc1f090d3efec276403f4b887006c3eea5b7a
ErgerSusha <ergersusha2@gmail.com>
2019-04-18 11:26:40 +0300

    More of validating and styling improvement

--- src/app/pages/add-lesson/add-lesson.page.ts
+++ src/app/pages/add-lesson/add-lesson.page.ts
@@ -3,6 +3,7 @@ import { StorageService } from 'src/app/services/storage/storage-service';
 import { AddLessonService } from 'src/app/services/http/add-lesson/add-lesson.service';
 import { AddSentenceService } from 'src/app/services/http/add-sentence/add-sentence.service';
 import { ToastController } from '@ionic/angular';
+import { color } from 'd3';
 
 @Component({
   selector: 'app-add-lesson',
@@ -16,6 +17,11 @@ export class AddLessonPage implements OnInit {
   sentenceText: string;
   sentenceHiddenWords: string;
 
+  lessonNameInputIsValidated: boolean = true;
+  lessonUrlInputIsValidated: boolean = true;
+  sentenceTextTextareaIsValidated: boolean = true;
+  sentenceWordsTextareaIsValidated: boolean = true;
+
   sentences: any[] = [];
 
   constructor(
@@ -26,78 +32,117 @@ export class AddLessonPage implements OnInit {
 
   ngOnInit() { }
 
+  onKeyLessonName(event: any) {
+    this.lessonNameInputIsValidated = event.target.value === "" ? false : true;
+  }
+
+  onKeyLessonUrl(event: any) {
+    this.lessonUrlInputIsValidated = event.target.value === "" ? false : true;
+  }
+
+  onKeySentenceText(event: any) {
+    this.sentenceTextTextareaIsValidated = event.target.value === "" ? false : true;
+  }
+
+  onKeySentenceWords(event: any) {
+    this.sentenceWordsTextareaIsValidated = event.target.value === "" ? false : true;
+  }
+
   async addNewSentenceToLesson() {
-    if (this.sentenceText !== undefined && this.sentenceHiddenWords !== undefined) {
-      this.sentenceHiddenWords = this.sentenceHiddenWords.replace(/\s/g, '');
-      const words = this.sentenceHiddenWords.split('|');
-      const indexesArray: Array<[number, number]> = [];
+    let validated: boolean = true;
 
-      for (const item of words) {
-        const indexes = item.split('-');
-        indexesArray.push([Number(indexes[0]), Number(indexes[1])]);
-      }
+    if (this.sentenceText === undefined || this.sentenceText === "") {
+      document.getElementById("sentence-text-textarea").style.borderColor = "#F00";
+      validated = false;
+    }
+    if (this.sentenceHiddenWords === undefined || this.sentenceHiddenWords === "") {
+      document.getElementById("sentence-words-textarea").style.borderColor = "#F00";
+      validated = false;
+    }
 
-      for (let i = 0; i < indexesArray.length - 1; i++) {
-        for (let j = i + 1; j < indexesArray.length; j++) {
-          if (!((indexesArray[i][0] < indexesArray[j][0] &&
-            indexesArray[i][0] + indexesArray[i][1] < indexesArray[j][0] + indexesArray[j][1]) ||
-            (indexesArray[i][0] > indexesArray[j][0] &&
+    if (!validated) return;
+
+    this.sentenceHiddenWords = this.sentenceHiddenWords.replace(/\s/g, '');
+    const words = this.sentenceHiddenWords.split('|');
+    const indexesArray: Array<[number, number]> = [];
+
+    for (const item of words) {
+      const indexes = item.split('-');
+      indexesArray.push([Number(indexes[0]), Number(indexes[1])]);
+    }
+
+    for (let i = 0; i < indexesArray.length - 1; i++) {
+      for (let j = i + 1; j < indexesArray.length; j++) {
+        if (!((indexesArray[i][0] < indexesArray[j][0] &&
+          indexesArray[i][0] + indexesArray[i][1] < indexesArray[j][0] + indexesArray[j][1]) ||
+          (indexesArray[i][0] > indexesArray[j][0] &&
             indexesArray[i][0] + indexesArray[i][1] > indexesArray[j][0] + indexesArray[j][1]))) {
-            return;
-          }
+          return;
         }
       }
-
-      this.sentences.push({
-        words: indexesArray,
-        text: this.sentenceText
-      });
-
-      this.sentenceText = "";
-      this.sentenceHiddenWords = "";
-
-      const toast = await this.toastController.create({
-        message: 'There are ' + this.sentences.length + ' sentences in current lesson',
-        position: 'bottom',
-        duration: 900,
-        animated: true
-      });
-      toast.present();
     }
+
+    this.sentences.push({
+      words: indexesArray,
+      text: this.sentenceText
+    });
+
+    this.sentenceText = "";
+    this.sentenceHiddenWords = "";
+
+    const toast = await this.toastController.create({
+      message: 'There are ' + this.sentences.length + ' sentences in current lesson',
+      position: 'bottom',
+      duration: 900,
+      animated: true
+    });
+    toast.present();
+
   }
 
   addNewLesson() {
-    if (this.lessonName !== undefined && this.lessonUrl !== undefined) {
-      this.storageService.get("user_id")
-        .then(userId => {
-          this.addLessonService.postNewLesson({
-            userId: userId,
-            name: this.lessonName,
-            url: this.lessonUrl
-          }).then(res => {
-            const newLessonId = res.id;
-            for (const sentence of this.sentences) {
-              this.addSentenceService.postNewSentence({
-                lessonId: newLessonId,
-                words: sentence.words,
-                text: sentence.text
-              });
-            }
-            this.lessonName = "";
-            this.lessonUrl = "";
-            this.sentenceText = "";
-            this.sentenceHiddenWords = "";
-            this.sentences = [];
-          });
-        }).then(async () => {
-          const toast = await this.toastController.create({
-            message: 'New lesson was added',
-            position: 'bottom',
-            duration: 700,
-            animated: true
-          });
-          toast.present();
-        });
+    let validated: boolean = true;
+
+    if (this.lessonName === undefined || this.lessonName === "") {
+      document.getElementById("lesson-name-input").style.borderColor = "#F00";
+      validated = false;
     }
+    if (this.lessonUrl === undefined || this.lessonUrl === "") {
+      document.getElementById("lesson-url-input").style.borderColor = "#F00";
+      validated = false;
+    }
+
+    if (!validated) return;
+
+    this.storageService.get("user_id")
+      .then(userId => {
+        this.addLessonService.postNewLesson({
+          userId: userId,
+          name: this.lessonName,
+          url: this.lessonUrl
+        }).then(res => {
+          const newLessonId = res.id;
+          for (const sentence of this.sentences) {
+            this.addSentenceService.postNewSentence({
+              lessonId: newLessonId,
+              words: sentence.words,
+              text: sentence.text
+            });
+          }
+          this.lessonName = "";
+          this.lessonUrl = "";
+          this.sentenceText = "";
+          this.sentenceHiddenWords = "";
+          this.sentences = [];
+        });
+      }).then(async () => {
+        const toast = await this.toastController.create({
+          message: 'New lesson was added',
+          position: 'bottom',
+          duration: 700,
+          animated: true
+        });
+        toast.present();
+      });
   }
 }
