7dd4f082cb1981719d6e201a2bc60271a0011cef
ErgerSusha <ergersusha2@gmail.com>
2019-05-14 10:36:08 +0300

    Imroved words selection, added http to save shared sentence

--- src/app/pages/sentence-adding-page/sentence-adding-page.page.ts
+++ src/app/pages/sentence-adding-page/sentence-adding-page.page.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { sharedText } from 'src/app/app.component';
 import { LessonsService } from 'src/app/services/lessons/lessons.service';
+import { AddSentenceService } from 'src/app/services/http/add-sentence/add-sentence.service';
+import { AddLessonService } from 'src/app/services/http/add-lesson/add-lesson.service';
+import { StorageService } from 'src/app/services/storage/storage-service';
+import { USER_ID_KEY } from 'src/app/services/auth/auth.service';
+import { NavController } from '@ionic/angular';
 
 const lastSelOffsets: Array<number> = [0, 0];
 const lastSelCoords: Array<number> = [0, 0];
@@ -17,7 +22,13 @@ export class SentenceAddingPagePage implements OnInit {
 	title: string;
 	sentence: string;
 
+	lessonId: any;
+
 	constructor(
+		private navCtrl: NavController,
+		private storageService: StorageService,
+		private addSentenceService: AddSentenceService,
+		private addLessonService: AddLessonService,
 		private route: ActivatedRoute,
 		private lessonsService: LessonsService) {
 		document.addEventListener('mouseup', this.showSelectionButton);
@@ -25,20 +36,43 @@ export class SentenceAddingPagePage implements OnInit {
 	}
 
 	ngOnInit() {
-		const lessonId = Number(this.route.snapshot.queryParamMap.get('lessonId'));
-		if (lessonId) {
-			this.title = this.lessonsService.getLessonByID(lessonId).name;
-		} else {
-			this.title = new Date().toJSON().slice(0, 10).replace(/-/g, '/') + ' ' + sharedText[0].substr(0, 10);
-		}
+		this.lessonId = Number(this.route.snapshot.queryParamMap.get('lessonId'));
+		this.lessonId ?
+			this.title = this.lessonsService.getLessonByID(this.lessonId).name :
+			this.title = new Date().toJSON().slice(0, 10).replace(/-/g, '/') +
+			' ' + sharedText[0].substr(0, 10);
 		this.sentence = sharedText[0];
 	}
 
 	submitSelections() {
-		if (this.indexesArray.length === 0) return;
-
+		if (this.indexesArray.length === 0)
+			return;
+		
 		this.indexesArray.sort((el1, el2) => el1[0] - el2[0]);
-		console.log(this.indexesArray);
+
+		if (this.lessonId) {
+			this.addSentenceService.postNewSentence({
+				lessonId: this.lessonId,
+				words: this.indexesArray,
+				text: this.sentence
+			});
+		} else {
+			this.storageService.get(USER_ID_KEY).then(userId => {
+				this.addLessonService.postNewLesson({
+					userId: userId,
+					name: this.title,
+					url: 'someurl@url.com'
+				}).then(res => {
+					const newLessonId = res.id;
+					this.addSentenceService.postNewSentence({
+						lessonId: newLessonId,
+						words: this.indexesArray,
+						text: this.sentence
+					});
+				});
+			});
+		}
+		this.navCtrl.navigateForward(['lessons-list']);
 	}
 
 	showSelectionButton() {
@@ -46,7 +80,7 @@ export class SentenceAddingPagePage implements OnInit {
 			if (window.getSelection().toString() === "") return;
 			const selection: any = window.getSelection().getRangeAt(0).getClientRects()[0];
 			const leftMargin: string = String(selection.x) + 'px';
-			const selectBtn = document.getElementById("select-btn");
+			const selectBtn = <HTMLDivElement>document.getElementsByClassName("select-btn")[0];
 			const btnStyle = selectBtn.style;
 			btnStyle.position = 'absolute';
 			btnStyle.display = 'block';
@@ -54,7 +88,8 @@ export class SentenceAddingPagePage implements OnInit {
 				leftMargin :
 				'calc(' + leftMargin + ' - 28vw)';
 			btnStyle.marginTop = String(selection.y + 50) + 'px';
-			
+			selectBtn.id = btnStyle.marginTop;
+
 			lastSelOffsets[0] = window.getSelection().getRangeAt(0).startOffset;
 			lastSelOffsets[1] = window.getSelection().getRangeAt(0).endOffset;
 
@@ -86,7 +121,7 @@ export class SentenceAddingPagePage implements OnInit {
 			if ((this.indexesArray[i][0] <= start &&
 				this.indexesArray[i][0] + this.indexesArray[i][1] >= start) ||
 				(this.indexesArray[i][0] <= finish &&
-				this.indexesArray[i][0] + this.indexesArray[i][1] >= finish)) {
+					this.indexesArray[i][0] + this.indexesArray[i][1] >= finish)) {
 				return;
 			}
 		}
@@ -108,14 +143,25 @@ export class SentenceAddingPagePage implements OnInit {
 		brdrStyle.width = String(lastSelCoords[2] + 6) + 'px';
 		brdrStyle.height = String(lastSelCoords[3] + 3) + 'px';
 		brdrStyle.position = 'absolute';
-		brdrStyle.backgroundColor = 'blue';
+		brdrStyle.background = 'blue';
 		brdrStyle.opacity = '0.4';
 		brdrStyle.borderRadius = '10px';
 		const borders = document.getElementsByClassName('border');
+		const textAreaBonds = document.getElementById('selectable-sentence-div').getBoundingClientRect();
 		document.getElementById('selectable-sentence-div').onscroll = () => {
-			Array.from(borders).forEach((brdr) => {
+			Array.from(borders).forEach((brdr: HTMLDivElement) => {
 				brdr.style.marginTop = 'calc(' + brdr.id + ' - ' + String(textArea.scrollTop) + 'px)';
+				const scrollPos = Number(brdr.style.marginTop.substr(5, brdr.style.marginTop.length - 8));
+				(scrollPos > textAreaBonds.bottom - Number(brdr.style.height.substr(0, brdr.style.height.length - 2)) || scrollPos < textAreaBonds.top) ?
+					brdr.style.background = 'none' :
+					brdr.style.background = 'blue';
 			});
+			const selBtn = <HTMLDivElement>document.getElementsByClassName("select-btn")[0];
+			selBtn.style.marginTop = 'calc(' + selBtn.id + ' - ' + String(textArea.scrollTop) + 'px)';
+			const scrollPos = Number(selBtn.style.marginTop.substr(5, selBtn.style.marginTop.length - 8));
+			(scrollPos > textAreaBonds.bottom || scrollPos < textAreaBonds.top) ?
+				selBtn.style.display = 'none' :
+				selBtn.style.display = 'block';
 		};
 		document.getElementsByTagName('app-sentence-adding-page')[0].appendChild(border);
 	}
