c166e9b82b8f53cb0d8a89e77e61eb1f64d94074
ErgerSusha <ergersusha2@gmail.com>
2019-06-27 17:33:57 +0300

    Version fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -165,7 +165,7 @@ export class AppComponent {
 
 	async showAbout() {
 		let header = 'EasyLang Context Tutor';
-		let message = '0.1.13.2';
+		let message = '0.1.14';
 		const alert = await this.alertCtrl.create({
 			header: header,
 			message: 'Version: ' + message,
