bc64dabfcb27f4e58c188d51dbc2d954c0c49ef4
ErgerSusha <ergersusha2@gmail.com>
2019-08-05 11:44:00 +0300

    Added correct pages lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuardService } from './services/guard/auth-guard.service';
-import { SentencesListPage } from './pages/sentences-list/sentences-list.page';
-import { SentenceGuessPage } from './pages/sentence-guess/sentence-guess.page';
-import { LoginPage } from './pages/login/login.page';
-import { AccountPage } from './pages/account/account.page';
-import { ForgetPage } from './pages/forget/forget.page';
-import { ChangePage } from './pages/change/change.page';
-import { AddLessonPage } from './pages/add-lesson/add-lesson.page';
-import { ShareAddingChoicePage } from './pages/share-adding-choice/share-adding-choice.page';
-import { SentenceAddingPage } from './pages/sentence-adding/sentence-adding.page';
-import { EditLessonTitlePage } from './pages/edit-lesson-title/edit-lesson-title.page';
 
 const routes: Routes = [
 	{ path: '', redirectTo: 'lessons-list', pathMatch: 'full' },
 	{ path: 'lessons-list', loadChildren: './pages/lessons-list/lessons-list.page.module#LessonsListPageModule', canActivate: [AuthGuardService] },
-	{ path: 'sentences-list', component: SentencesListPage, canActivate: [AuthGuardService] },
-	{ path: 'sentence-guess', component: SentenceGuessPage, canActivate: [AuthGuardService]  },
-	{ path: 'login', component: LoginPage },
-	{ path: 'account', component: AccountPage },
-	{ path: 'forget', component: ForgetPage },
-	{ path: 'change', component: ChangePage },
-	{ path: 'add-lesson', component: AddLessonPage, canActivate: [AuthGuardService]  },
-	{ path: 'share-adding-choice', component: ShareAddingChoicePage, canActivate: [AuthGuardService]  },
-	{ path: 'sentence-adding', component: SentenceAddingPage, canActivate: [AuthGuardService]  },
-	{ path: 'edit-lesson-title', component: EditLessonTitlePage, canActivate: [AuthGuardService]  }
+	{ path: 'sentences-list', loadChildren: './pages/sentences-list/sentences-list.page.module#SentencesListPageModule' },
+	{ path: 'sentence-guess', loadChildren: './pages/sentence-guess/sentence-guess.module#SentenceGuessPageModule'  },
+	{ path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+	{ path: 'account', loadChildren: './pages/account/account.module#AccountPageModule' },
+	{ path: 'forget', loadChildren: './pages/forget/forget.module#ForgetPageModule' },
+	{ path: 'change', loadChildren: './pages/change/change.module#ChangePageModule' },
+	{ path: 'add-lesson', loadChildren: './pages/add-lesson/add-lesson.module#AddLessonPageModule' },
+	{ path: 'share-adding-choice', loadChildren: './pages/share-adding-choice/share-adding-choice.module#ShareAddingChoicePageModule' },
+	{ path: 'sentence-adding', loadChildren: './pages/sentence-adding/sentence-adding.module#SentenceAddingPageModule' },
+	{ path: 'edit-lesson-title', loadChildren: './pages/edit-lesson-title/edit-lesson-title.module#EditLessonTitlePageModule' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,18 +14,8 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { Storage, IonicStorageModule } from '@ionic/storage';
 import { JwtModule, JWT_OPTIONS } from '@auth0/angular-jwt';
-import { LoginPageModule } from './pages/login/login.module';
-import { AccountPageModule } from './pages/account/account.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SentencesListPageModule } from './pages/sentences-list/sentences-list.page.module';
-import { SentenceGuessPageModule } from './pages/sentence-guess/sentence-guess.module';
-import { ForgetPageModule } from './pages/forget/forget.module';
-import { ChangePageModule } from './pages/change/change.module';
 import { environment } from 'src/environments/environment';
-import { AddLessonPageModule } from './pages/add-lesson/add-lesson.module';
-import { ShareAddingChoicePageModule } from './pages/share-adding-choice/share-adding-choice.module';
-import { SentenceAddingPageModule } from './pages/sentence-adding/sentence-adding.module';
-import { EditLessonTitlePageModule } from './pages/edit-lesson-title/edit-lesson-title.module';
 import { Globals } from './services/globals/globals';
 import { ShareLessonModal } from './modals/share-lesson/share-lesson.modal';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
@@ -48,17 +38,7 @@ export function jwtOptionsFactory(storage) {
 		ReactiveFormsModule,
 		FormsModule,
 		HttpClientModule,
-		SentencesListPageModule,
-		SentenceGuessPageModule,
-		AddLessonPageModule,
-		ShareAddingChoicePageModule,
-		SentenceAddingPageModule,
-		EditLessonTitlePageModule,
-		LoginPageModule,
 		BrowserAnimationsModule,
-		AccountPageModule,
-		ForgetPageModule,
-		ChangePageModule,
 		IonicStorageModule.forRoot({
 			name: 'easy-db',
 			driverOrder: ['indexeddb', 'sqlite', 'websql']
